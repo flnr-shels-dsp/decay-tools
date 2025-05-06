03ecb32e9d02017f6aa55ceaed3163ffeb8be677
Roman Mukhin <59999203+rm1113@users.noreply.github.com>
2025-05-06 20:59:31 +0200

    rename

--- src/decay_tools/fit.py
+++ src/decay_tools/fit.py
@@ -51,14 +51,14 @@ def _log_curve_zero_bkg(logt: np.ndarray, lamb: float, n: float) -> np.ndarray:
     return n * np.exp(logt + np.log(lamb)) * np.exp(-np.exp(logt + np.log(lamb)))
 
 
-def shmidt(logt, lamb, n, c) -> np.ndarray:
+def schmidt(logt, lamb, n, c) -> np.ndarray:
     """
     # TODO
     """
     return _log_curve_zero_bkg(logt, lamb=lamb, n=n) + c
 
 
-def double_shmidt(logt, l1, n1, l2, n2, c):
+def double_schmidt(logt, l1, n1, l2, n2, c):
     """
     # TODO
     """
@@ -111,7 +111,7 @@ def estimate_n_bins(
     return n_bins
 
 
-def fit_single_shmidt(
+def fit_single_schmidt(
     logt: Iterable,
     initial_guess: DecayParameters,
     bounds: tuple[DecayParameters | int, DecayParameters | int] | None = None,
@@ -145,7 +145,7 @@ def fit_single_shmidt(
         _bounds.append(b)
     
     [l, n, c], pcov = op.curve_fit(
-        shmidt,
+        schmidt,
         bins,
         data,
         p0=initial_guess.to_lnc(),
@@ -172,7 +172,7 @@ def fit_single_shmidt(
         try:
             if any(data < 10):
                 print("Warning! Some categories have less than 10 counts, chi-square test could be not representative!")
-            chi = st.chisquare(data, shmidt(bins, l, n, c), ddof=chi_square_nddof)
+            chi = st.chisquare(data, schmidt(bins, l, n, c), ddof=chi_square_nddof)
             if chi.pvalue > 0.05:
                 print("Good fit!")
             else:
@@ -197,13 +197,13 @@ def visualize_single_fit(
     x = np.linspace(bins.min(), bins.max(), 100)
     plt.errorbar(x=bins, y=data, yerr=np.sqrt(data), fmt='o')
     [l,n,c] = decay_parameters.to_lnc()
-    plt.plot(x, shmidt(x, l, n, c))
+    plt.plot(x, schmidt(x, l, n, c))
     plt.xlabel(r'$ln_{\Delta T}$')
     plt.ylabel('count/channel')
     plt.show()
 
 
-# def fit_double_shmidt(df, n_bins, pts_drop, nddof=5):
+# def fit_double_schmidt(df, n_bins, pts_drop, nddof=5):
 #     d, b = np.histogram(df.logt, bins=n_bins)
 #     dt = np.diff(b)[0]
 #     print(f'Bin size: {dt}')
@@ -214,7 +214,7 @@ def visualize_single_fit(
 #     d = d[pts_drop:]
 #     #     print(bb)
 #     #     print(dd)
-#     [l1, n1, l2, n2, c], pcov = op.curve_fit(double_shmidt, b, d / dt, p0=[0.2, 500, 0.02, 10, 1.0],
+#     [l1, n1, l2, n2, c], pcov = op.curve_fit(double_schmidt, b, d / dt, p0=[0.2, 500, 0.02, 10, 1.0],
 #                                              bounds=(0, [1, 3e3, 1, 2e3, 1e6]), method='trf')
 
 #     perr = np.sqrt(pcov.diagonal())
@@ -223,9 +223,9 @@ def visualize_single_fit(
 #     plt.errorbar(x=bb[:pts_drop], y=dd[:pts_drop], yerr=np.sqrt(dd[:pts_drop]), c='r', fmt='o')
 #     plt.errorbar(x=b, y=d, yerr=np.sqrt(d), fmt='o')
 
-#     plt.plot(x, double_shmidt(x, l1, n1, l2, n2, c) * dt);
-#     plt.plot(x, shmidt(x, l1, n1, c) * dt)
-#     plt.plot(x, shmidt(x, l2, n2, c) * dt)
+#     plt.plot(x, double_schmidt(x, l1, n1, l2, n2, c) * dt);
+#     plt.plot(x, schmidt(x, l1, n1, c) * dt)
+#     plt.plot(x, schmidt(x, l2, n2, c) * dt)
 #     plt.xlabel(r'$ln{\Delta T(ER-SF)}, ln(\mu s)$')
 #     plt.ylabel('count/channel')
 #     #     plt.savefig('ActivitiesNo250_22.png', dpi=500)
@@ -247,7 +247,7 @@ def visualize_single_fit(
 #     nu = n2 / (n1 + n2)
 #     dnu = (dn1 * dn1 * n2 * n2 + dn2 * dn2 * n1 * n1) ** 0.5 / (n1 + n2) ** 2
 
-#     func = double_shmidt(b, l1, n1, l2, n2, c) * dt
+#     func = double_schmidt(b, l1, n1, l2, n2, c) * dt
 #     xx = np.linspace(b.min(), b.max(), 100)
 
 #     print(f"l1 = {l1}, l2 = {l2}")
@@ -255,7 +255,7 @@ def visualize_single_fit(
 #     print(f'n0 short = {n1:.1f}+-{dn1:.1f}, n0 long = {n2:.1f}+-{dn2:.1f}')
 #     print(f"background constant = {c:.3f}+-{perr[-1]:.3f}")
 #     print(f"isomer/ground = {nu:.3f}+-{dnu:.3f}")
-#     print(f"# events observed = {d.sum()}, integral = {np.trapz(y=double_shmidt(xx, l1, n1, l2, n2, c), x=xx)}")
+#     print(f"# events observed = {d.sum()}, integral = {np.trapz(y=double_schmidt(xx, l1, n1, l2, n2, c), x=xx)}")
 #     print("chi-square", st.chisquare(d, func, ddof=nddof))
 
 
@@ -264,5 +264,5 @@ if __name__ == "__main__":
     times_mks = np.array([2, 2, 2.1, 2.1, 2.7, 2.8, 2.8, 2.8, 2.8, 3, 3.4, 3.5, 3.6, 3.6, 4, 4, 4, 4.1, 4.3, 4.4, 4.6, 4.7, 5, 5.3, 5.3, 5.5, 5.7, 5.9, 6.3, 6.5, 6.7, 6.7, 7, 7.3, 7.4, 7.8, 7.9, 8.2, 8.3, 8.3, 8.5, 9, 10.2, 10.3, 10.4, 10.7, 11.2, 11.2, 11.6, 11.6, 11.7, 12.1, 12.5, 13.1, 13.2, 13.3, 13.6, 13.7, 13.8, 13.8, 16, 16.8, 19.7, 20.3, 21.5, 21.5, 21.6, 22.2, 23.2, 25.5, 26.7, 27.6, 30.6, 31.8, 31.8, 32.1, 34.6, 44.9, 44.9, 47.6, 59, 76.3, 80.8, 95.2])
     times_ln = np.log(times_mks)
     guess = DecayParameters(10, 100, 0)
-    res = fit_single_shmidt(times_ln, initial_guess=guess)
+    res = fit_single_schmidt(times_ln, initial_guess=guess)
     visualize_single_fit(logt=times_ln, decay_parameters=res)
